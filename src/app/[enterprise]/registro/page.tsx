70801eb82433d5bd2d0126a4ff514d4db4ae8157
emxanuel <ealg0528@gmail.com>
2024-09-28 00:17:41 -0400

    edit params in createPatient function

---
+++ src/app/[enterprise]/registro/page.tsx
@@ -0,0 +1,16 @@
+import MainForm from "@/features/registro/components/MainForm";
+import styles from './styles.module.css'
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Registro"
+}
+
+export default function Page() {
+  return (
+    <div className={styles.container}>
+      <h1 className="text-2xl font-bold">Registro para consulta</h1>
+      <MainForm />
+    </div>
+  );
+}
--- src/features/registro/actions/createPatient.ts
+++ src/features/registro/actions/createPatient.ts
@@ -2,7 +2,7 @@ import api from "@/api";
 import { convertFormData } from "../helpers/convertTypes";
 import { FormData } from "@/types";
 
-export const createPatient = async (data: FormData) => {
+export const createPatient = async (data: FormData, isAdmin: boolean = true, enterpriseId: number) => {
     const {insurance, patient, emergencyContact, visit} = convertFormData(data);
     try {
         const response = await api.post("/patients", {
@@ -10,6 +10,8 @@ export const createPatient = async (data: FormData) => {
             patientInfo: patient,
             emergencyContactInfo: emergencyContact,
             visitInfo: visit,
+            isAdmin,
+            enterpriseId
         });
         return response.data;
     } catch (error) {
